34f45d5464ae72fe71ae18bd5a80848568a78da5
nazhG <nazhirgonzalez@gmail.com>
2024-09-26 01:55:39 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ajusta las rutas según tu estructura de proyecto
+    theme: {
+      extend: {
+        colors: {
+          primary: '#375F80',
+          secondary: '#545454',
+        },
+      },
+    },
+    plugins: [],
+  };
