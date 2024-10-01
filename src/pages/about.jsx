1fe5077742f2b3f1e5edcb437321c910b2d6f728
nazhG <nazhirgonzalez@gmail.com>
2024-10-01 13:52:21 -0500

    🎨 responsive

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,10 +7,10 @@ const About = () => {
             <br />
             <section className="flex flex-col items-center font-poppins">
                 <h2 className="font-semibold text-xl mb-2">The Problem</h2>
-                <h4 className="text-primary mb-12 text-center sm:px-2">Overburdened Therapists, Underserved Patients</h4>
+                <h4 className="text-primary mb-12 text-center sm:px-6">Overburdened Therapists, Underserved Patients</h4>
                 <div className="flex max-w-[1440px] gap-12 mb-20">
                     <img className="w-2/5 md:hidden" src="https://s3-alpha-sig.figma.com/img/5474/a6b1/8be1fcc54af0c5f4934e6d940c1fece9?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ERBGW6aVqQhEGujLjbYEM02KwdSSCU10rNAlyThZPFgRruTAUUZ2Nw7qLoGXAMGdEI-EZoZb2rXCuUZ38rmeSR0xyqkGazbFMXOF9bnflox3SpCjby592BEi9TYQQYzu71kGBCCLtJ-hShHtealCnNyPLevHEvoRFcjokELsVRVhsMPT7figvUzsNvdSMwqaw5MfeuBjcckf7h-ZGB14ObvfKJQ5l~6p0Jnt1i6Vd6PqIUrAIHvpjmSmxapGuA1DDt3zgV3vKMIR1bI-k3YSNEWUiESZ0lw2DJ2cSmV4mX9EDDZGj7nqwbO9sEnfC-u4QCY12QgOl3SgU3LL64dnRw__" alt="Files" />
-                    <div className="w-3/5 md:w-full md:px-5 flex flex-col">
+                    <div className="w-3/5 md:w-full md:px-8 flex flex-col">
                         <p className="font-semibold mb-4">Challenges for Mental Health Professionals</p>
                         <ul className="list-disc h-full flex flex-col justify-between">
                             <li className="mb-1">High demand with limited resources</li>
@@ -52,11 +52,11 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col justify-center text-center w-full mb-8 px-20">
+                <div className="flex flex-col justify-center text-center w-full mb-8 px-20 sm:px-8">
                     <h4 className="font-semibold mb-2 text-2xl">Haiva Solution</h4>
                     <p className="text-primary mb-14">AI-Powered Mental Health Assistant</p>
                     <div className="flex md:flex-col gap-6 mb-16 items-center">
-                        <span className="w-1/4 font-semibold text-3xl">What is <span className="text-light">haiva</span>?</span>
+                        <span className="w-1/4 sm:w-full font-semibold text-3xl">What is <span className="text-light">haiva</span>?</span>
                         <div className="border-black border-l-[1px] border-b-[1px] h-28 md:h-[1px] w-[1px] md:w-full"></div>
                         <span className="w-3/4 font-light">Haiva is an AI-powered platform that supports mental health professionals by automating transcription, providing acoustic and emotional analysis, offering predictive analytics, and integrating seamlessly with existing workflows.</span>
                     </div>
